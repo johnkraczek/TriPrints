674c7c6ee62b8ed6f4ece3d21aaf782b861fc03e
John <john.kraczek@gmail.com>
2014-08-12 14:19:08 -0600

    Added Make File
    This file will make all the pieces and put them in the STLs File.

---
+++ Tricopterv2/make.bat
@@ -0,0 +1,10 @@
+PATH = %PATH%;C:\Program Files\OpenSCAD\
+openscad.com -o Stls\ESC.stl Booms\ESC.scad
+openscad.com -o Stls\HousingEnd.stl Booms\HousingEnd.scad
+openscad.com -o Stls\housing.stl Booms\housing.scad
+openscad.com -o Stls\pivot.stl Booms\pivot.scad
+openscad.com -o Stls\PivotJig.stl Booms\PivotJig.scad
+openscad.com -o Stls\Transmitter.stl Booms\Transmitter.scad
+openscad.com -o Stls\Nose.stl Body\Nose.scad
+openscad.com -o Stls\Tail.stl Booms\tail.scad
+pause
